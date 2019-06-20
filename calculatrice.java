fcb42a8ff9a6a35e566ab836fd905f78a88ea839
Votre nom ou pseudo <nguemajaecan@gmail.com>
2019-06-21 01:51:23 +0200

    Ajout du fichier calculatrice.java

---
+++ calculatrice.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;  
+public class Calculatrice {
+
+	static Scanner clavier = new Scanner(System.in);
+	public static void main(String[] args) {
+		int nbre1 = 0, nbre2 = 0;
+		
+		System.out.println("Entrer le nombre 1:");
+		nbre1 = clavier.nextInt();
+		
+		System.out.println("Entrer le nombre 2:");
+		nbre2 = clavier.nextInt();
+		
+		
+		System.out.println("la somme de " + nbre1 + " et " + nbre2 + " est: " + (nbre1+nbre2));
